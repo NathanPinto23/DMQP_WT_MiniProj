94085454a2cb530136376001323af0540715cc10
NathanPinto23 <143392746+NathanPinto23@users.noreply.github.com>
2024-07-30 23:20:57 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+
+****************************
+******* login.php **********
+***
+*************************
+
+
+PHP script to handle/validate a login and create a session (and cookie if required)
+
+*/
+
+
+
+session_start();
+
+
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+    $username = $_POST["username"];
+
+    $password = $_POST["password"];
+
+    
+    if($username=="user" && $password=="password"){
+
+        $_SESSION["loggedin"]=true;
+
+        $_SESSION["username"]=$username;
+
+    }
+
+}
+
+
+?>
