a6a1660ec212cf9b3b982c9b1c6e1b84dd3dc05e
NathanPinto23 <143392746+NathanPinto23@users.noreply.github.com>
2024-07-30 22:33:13 +0530

    Add files via upload

---
+++ script-index.js
@@ -0,0 +1,28 @@
+// script-index.js
+document.addEventListener("DOMContentLoaded", function() {
+
+	function getCookie(name) {
+
+		const value = `; ${document.cookie}`;
+
+		const parts = value.split(`; ${name}=`);
+
+		if (parts.length === 2) return parts.pop().split(';').shift();
+
+	}
+
+
+
+	// Check if the user is logged in
+
+	const isLoggedIn = getCookie("isLoggedIn");
+
+	if (!isLoggedIn) {
+
+		// Redirect to the login page
+
+		window.location.href = "login.html";
+
+	}
+
+});
